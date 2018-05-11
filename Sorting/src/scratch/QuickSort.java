9a4ad3b496ea131d4e0d3384ee21e68a9b71eb61
Josh Archer <jarcher@greenriver.edu>
2018-05-11 12:52:47 -0700

    Added some basic functions to help us write quick sort.

---
+++ Sorting/src/scratch/QuickSort.java
@@ -0,0 +1,44 @@
+package scratch;
+
+import java.util.Random;
+
+public class QuickSort
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static void swap(int[] array, int first, int second)
+    {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+
+    private static boolean detectAdjacentInversions(int[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int[] generateRandomArray(int size, int low, int high)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = low + random.nextInt(high - low + 1);
+        }
+
+        return array;
+    }
+}
