76634e11efa33a439e1d2366e15e7a34c34e2df8
Josh Archer <jarcher@greenriver.edu>
2018-05-11 13:22:12 -0700

    Added a version of quick sort that uses the left-most elements as a pivot.

--- Sorting/src/divide_and_conquer/QuickSort.java
+++ Sorting/src/divide_and_conquer/QuickSort.java
@@ -1,12 +1,72 @@
 package divide_and_conquer;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSort
 {
     public static void main(String[] args)
     {
+        int[] data = generateRandomArray(10000, 1, 10000);
+        quickSort(data);
+        System.out.println(Arrays.toString(data));
+        System.out.println("Sorted? " + !detectAdjacentInversions(data));
+    }
 
+    //sorts our array
+    public static void quickSort(int[] array)
+    {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    //sorts a sub-array from low to high
+    private static void quickSort(int[] array, int low, int high)
+    {
+        if (low >= high)
+        {
+            return;
+        }
+
+        //pick my pivot
+        int pivot = array[low];
+
+        //partition my data
+        int i = low, j = high;
+        while (i <= j)
+        {
+            //look for an element larger than the pivot
+            while (array[i] < pivot)
+            {
+                i++;
+            }
+
+            //look for an element smaller than the pivot
+            while (array[j] > pivot)
+            {
+                j--;
+            }
+
+            //if the pointers haven't "crossed" one another
+            if (i <= j)
+            {
+                swap(array, i, j);
+
+                //move the pointers forward
+                i++;
+                j--;
+            }
+        }
+
+        //recursively sort the left and right sub-array
+        if (low < j)
+        {
+            quickSort(array, low, j);
+        }
+
+        if (high > i)
+        {
+            quickSort(array, i, high);
+        }
     }
 
     private static void swap(int[] array, int first, int second)
